5f405eae5d9e40e554f94c9ce2b2b8c7cb52bcd9
Yeison Montoya <yeisondiseno@gmail.com>
2024-07-07 17:30:33 -0500

    first style configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 import MillionLint from "@million/lint";
+import path from "path"
 
-const nextConfig = {};
+const __dirname = path.resolve();
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles/")],
+  },
+};
 
 
 export default MillionLint.next({ rsc: true })(nextConfig);
