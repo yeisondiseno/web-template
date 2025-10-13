97142d22a601fa833a5c1096d8110e7ec3d9ae4a
yeisondiseno <yeisondiseno@gmail.com>
2025-10-13 18:36:06 -0500

    feat: use the query of the image to manage the render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ const HomePage = () => {
         <Card>
           <ImgCSR
             src={
-              'https://images.unsplash.com/photo-1759772238808-6ce358fab418?q=80&w=1123&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              'https://images.unsplash.com/photo-1759772238808-6ce358fab418?q=80&w=300h=250&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             }
             alt='Example'
             width={300}
@@ -109,7 +109,7 @@ const HomePage = () => {
         <Card>
           <ImgCSR
             src={
-              'https://images.unsplash.com/photo-1755397467747-6160983f9694?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              'https://images.unsplash.com/photo-1755397467747-6160983f9694?q=80&w=300&h=250&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             }
             alt='Example'
             width={300}
@@ -127,7 +127,7 @@ const HomePage = () => {
         <Card>
           <ImgCSR
             src={
-              'https://images.unsplash.com/photo-1759221793465-4795ba2eaafc?q=80&w=1122&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              'https://images.unsplash.com/photo-1759221793465-4795ba2eaafc?q=80&w=300&h=250&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             }
             alt='Example'
             width={300}
